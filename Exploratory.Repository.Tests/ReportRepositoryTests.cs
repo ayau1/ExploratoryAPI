2dac19fa261a6a8ff992bd4c9c452fb70c0ec399
ayau1 <amie.yau@bglgroup.co.uk>
2017-08-30 10:48:06 +0100

    added test project

---
+++ Exploratory.Repository.Tests/ReportRepositoryTests.cs
@@ -0,0 +1,36 @@
+﻿using Exploratory.Domain.Models;
+using Exploratory.Repository.RepoCore;
+using Exploratory.Repository.Repositories;
+using NUnit.Framework;
+using NSubstitute;
+
+namespace Exploratory.Repository.Tests
+{
+    class ReportRepositoryTests
+    {
+        private IMongoProvider _mongoProvider;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mongoProvider = Substitute.For<IMongoProvider>();
+        }
+
+        [Test]
+        public void ShouldCallMongoProvidersInsertMethod_When_SaveIsCalledFromReportRepository()
+        {
+            //Arrange
+            var reportRepo = new ReportRepository(_mongoProvider);
+            var report = new Report();
+            
+            //Act
+            reportRepo.SaveReport(report);
+            
+            //Assert
+            //we are asserting mongo provider is called
+            _mongoProvider.Received(1).Insert(Arg.Any<Report>());
+        }
+
+        // create a test for report controller. It should call report repository.savereport when add is called from the controller. You need a new test project for ExploratoryAPI
+    }
+}
